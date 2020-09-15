80124b1fbfa0c7a2f0ef92284f25c2a073fb8b00
Santiago Sanmartin <santiago.sanmartin@udea.edu.co>
2020-09-15 16:44:26 -0500

    FEAT: Se crean scripts iniciales para seleccionar letra aleatoria

---
+++ Assets/Scripts/Musical-gameplay/GameplayOrchester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayOrchester : MonoBehaviour
+{
+    public GameObject TextDisplayer;
+    public int type = 1;
+
+    private void Awake()
+    {
+        if (this.type.Equals(1))
+        {
+            this.BeginOne();
+        }
+    }
+
+    private void BeginOne()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Musical-gameplay/TextDisplayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDisplayer : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public Image image;
+    public List<string> CharacterPossibilities = new LinkedList<string>();
+    public int selectedCharacterIndex;
+    public string selectedCharacter;
+
+    public string randomlyGenerateCharacter()
+    {
+        this.selectedCharacterIndex = Random.Range(0, this.CharacterPossibilities.Count);
+        this.selectedCharacter = this.CharacterPossibilities[this.selectedCharacterIndex];
+        return this.selectedCharacter;
+    }
+
+}
