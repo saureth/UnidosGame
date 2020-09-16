334c1d5b4076d3a4a6e1aed3fdd26ae8f48f84ce
Santiago Sanmartin <santiago.sanmartin@udea.edu.co>
2020-09-16 13:53:55 -0500

    FEAT: Se implementa jugabilidad propuesta #2: parecida a Guitar Hero

---
+++ Assets/Scripts/Musical-gameplay/Guitar-Hero-Approach/Token.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token : MonoBehaviour
+{
+
+    public float upDownSpeed;
+    public float lifeTime;
+    float deathHour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deathHour = Time.time + lifeTime;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.down * upDownSpeed * Time.fixedDeltaTime);
+        if (Time.time > deathHour)
+        {
+            print("¡Incorrecto!");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Musical-gameplay/Guitar-Hero-Approach/TokenCreator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TokenCreator : MonoBehaviour
+{
+    public GameObject tokenPrefab;
+    public Transform[] spawnPoints;
+    public Spawner[] spawners;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator Spawn()
+    {
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            yield return new WaitForSeconds(spawners[i].delay);
+            Instantiate(tokenPrefab, spawnPoints[spawners[i].spawnerIndex].position, Quaternion.identity);
+        }
+    }
+
+}
+
+
+[System.Serializable]
+public class Spawner
+{
+    public float delay;
+    public int spawnerIndex;
+}
