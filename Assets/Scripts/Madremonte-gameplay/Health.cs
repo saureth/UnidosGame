a180565ba1429cff1a22b59b4ec546de55b21af9
Gabriel <gabrielcamargogarcia@gmail.com>
2020-09-30 13:14:45 -0500

    Add health script

---
+++ Assets/Scripts/Madremonte-gameplay/Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float maximumHealth;
+    public float actualHealth;
+    public float healthPercentage;
+    public Slider uiHealthPercentage;
+    public UnityEvent death;
+    void Start()
+    {
+        actualHealth = maximumHealth;
+        healthPercentage = 1f;
+    }
+    public void DropHealth(float dmgValue)
+    {
+        
+        actualHealth = Mathf.Clamp(actualHealth - dmgValue, 0, maximumHealth);
+        UpdateHealth();
+        if(healthPercentage == 0)
+        {
+            death.Invoke();
+        }
+    }
+    public void RecoverHealth(float healthValue)
+    {
+        actualHealth = Mathf.Clamp(actualHealth + healthValue, 0, maximumHealth);
+        UpdateHealth();
+    }
+    public void UpdateHealth()
+    {
+        healthPercentage = actualHealth / maximumHealth;
+        if (uiHealthPercentage != null)
+        {
+            uiHealthPercentage.value = healthPercentage;
+        }
+    }
+}
