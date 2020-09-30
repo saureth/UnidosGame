ada41386fe0b2f42c342b76279e1c7d3beb99b27
Morion <adrian.martinez@udea.edu.co>
2020-09-30 13:10:11 -0500

    Escena de buscar mula y rayo de la muerte para la bruja

---
+++ Assets/Scripts/Bruja/RayoDeLaMuerte.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(LineRenderer))]
+public class RayoDeLaMuerte : MonoBehaviour
+{
+    private LineRenderer linea;
+    public GameObject particulas;
+
+    private void Start()
+    {
+        linea = GetComponent<LineRenderer>();
+        linea.SetPosition(0, transform.position);
+        linea.SetPosition(1, transform.position);
+    }
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward, out hit))
+        {
+            linea.SetPosition(1, hit.point);
+            particulas.SetActive(true);
+            particulas.transform.position = hit.point;
+        }
+        else
+        {
+            particulas.SetActive(false);
+            linea.SetPosition(1, transform.position + transform.forward*100);
+        }
+    }
+}
