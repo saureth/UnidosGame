5212884fe1bee55a7f8e2c2d67687d9e41ce355a
Santiago Sanmartin <santiago.sanmartin@udea.edu.co>
2020-09-30 16:35:59 -0500

    FEAT: Se implementa comportamiento del altar

---
+++ Assets/Scripts/Bruja/AltarBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarBehaviour : MonoBehaviour
+{
+    public Health brujaRef = null;
+    public float cooldown = 60f;
+    public bool hit = false;
+    public float hitDamage = 1f;
+
+    private void Awake()
+    {
+        this.brujaRef = GameObject.FindGameObjectWithTag("Bruja").GetComponent<Health>();
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && !hit && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Le pegó con la gema");
+            hit = true;
+            this.brujaRef.DropHealth(hitDamage);
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        hit = false;
+    }
+
+}
