b76675a1c7791650dbb19a13bae8594c37ce573f
Cristian <ccamilo.bedoya@gmail.com>
2020-09-29 20:55:53 -0500

    Init escena de runner

---
+++ Assets/Scripts/Runner-gameplay/TouchTrap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TouchTrap : MonoBehaviour
+{
+    
+    public LayerMask playerLayer;
+    public PlayerRunnerBehaviour pl;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (((1 << col.gameObject.layer) & playerLayer) != 0)
+        {
+            pl.TakeDamage();
+        }
+    }
+}
