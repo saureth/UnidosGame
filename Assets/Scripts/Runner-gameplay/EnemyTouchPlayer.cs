a3ca14255738d72620c41f68bbf6072bacdf03ff
Cristian <ccamilo.bedoya@gmail.com>
2020-09-30 22:42:24 -0500

    Initial enemy behaviour for runner

---
+++ Assets/Scripts/Runner-gameplay/EnemyTouchPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTouchPlayer : MonoBehaviour
+{
+    public PlayerRunnerBehaviour pl;
+    public LayerMask playerLayer;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (((1 << col.gameObject.layer) & playerLayer) != 0)
+        {
+            pl.TakeDamage();
+            // animar madremonte para hacer daño
+        }
+    }
+
+
+}
