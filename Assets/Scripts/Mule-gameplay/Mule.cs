4f67cf95b785f2c7a3951a48a8c64b7baa56ece8
Gabriel <gabrielcamargogarcia@gmail.com>
2020-10-02 01:25:51 -0500

    Add Mule script

---
+++ Assets/Scripts/Mule-gameplay/Mule.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Mule : MonoBehaviour
+{
+    public UnityEvent foundMule;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            foundMule.Invoke();
+        }
+    }
+}
