c2e00ae9e1900f015dcc40506ea12b8ad310463c
Morion <adrian.martinez@udea.edu.co>
2020-10-02 21:24:56 -0500

    CanvasCorrer

--- Assets/Scripts/Varios/Gema.cs
+++ Assets/Scripts/Varios/Gema.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gema : MonoBehaviour
 {
@@ -24,7 +25,7 @@ public class Gema : MonoBehaviour
 
     void DesactivarGema()
     {
-
+        SceneManager.LoadScene("RunnerScene");
         gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Varios/UICorrer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICorrer : MonoBehaviour
+{
+    public Sprite spIngles;
+    public Image imVer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = PlayerPrefs.GetInt("idioma", 0);
+        if (i!=0)
+        {
+            imVer.sprite = spIngles;
+        }
+    }
+
+}
